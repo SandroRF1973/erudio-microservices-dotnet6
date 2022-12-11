e4a45e0ce48bd3b11bd6a6012a8fd19d53cbbfc4
Sandro Ferreira <sandrorf1973@gmail.com>
2022-12-11 14:35:06 -0300

    ok

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,22 @@
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
