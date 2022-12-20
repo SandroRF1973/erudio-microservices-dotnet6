52b977e0123752629e1ab8d78982c8661300c4c4
Sandro Ferreira <sandrorf1973@gmail.com>
2022-12-20 20:52:46 -0300

    ok

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -38,7 +38,7 @@ public class ProductController : ControllerBase
     }
     
     [HttpPost]
-    public async Task<ActionResult<ProductVO>> Create(ProductVO vo) 
+    public async Task<ActionResult<ProductVO>> Create([FromBody]ProductVO vo) 
     {
         if (vo == null)
         {
@@ -51,7 +51,7 @@ public class ProductController : ControllerBase
     }
 
     [HttpPut]
-    public async Task<ActionResult<ProductVO>> Update(ProductVO vo) 
+    public async Task<ActionResult<ProductVO>> Update([FromBody]ProductVO vo) 
     {
         if (vo == null)
         {
